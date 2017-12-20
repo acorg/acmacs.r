1b8ec2f97a392512701035a23d9bfcdb95e687ea
Eugene Skepner <github@skepner.eu>
2017-12-20 15:31:59 +0100

    projection support

--- tests/testthat/test-04-projections.R
+++ tests/testthat/test-04-projections.R
@@ -3,9 +3,12 @@ context("Test chart$projections")
 test_chart <- function(filename, expected_num_projections, expected_stress) {
     chart <- new(acmacs.Chart, filename)
 
+    layout <- chart$projections[[1]]$layout
+
     test_that("number of projections", { expect_equal(chart$number_of_projections, expected_num_projections) })
     test_that("stress", { expect_equal(chart$projections[[1]]$stress, expected_stress) })
     test_that("minimum_column_basis", { expect_equal(chart$projections[[1]]$minimum_column_basis, "none") })
+    test_that("layout vs. transformed_layout vs. transformation", { expect_equal(chart$projections[[1]]$layout %*% chart$projections[[1]]$transformation, chart$projections[[1]]$transformed_layout) })
 
 }
 
