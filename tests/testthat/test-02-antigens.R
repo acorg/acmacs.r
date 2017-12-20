a9d40b8fc88bb552c3bcd6632b7fe9564d449ab7
Eugene Skepner <github@skepner.eu>
2017-12-20 08:53:55 +0100

    tests development

--- tests/testthat/test-02-antigens.R
+++ tests/testthat/test-02-antigens.R
@@ -4,12 +4,15 @@ ad_antigen_list <- system("ad chart-names test-2004-3.ace | grep '^AG'", intern=
 #write(antigen_list, stderr())
 
 chart <- new(acmacs.Chart, "test-2004-3.ace")
-test_that("number of antigens", { expect_equal(chart$number_of_antigens, 22) })
 
 ags <- chart$antigens
-# write(paste(sapply(ags, function(a) { paste("AG", a$name, collapse=" ") }), collapse="\n"), stderr())
-r_antigen_list <- paste(sapply(1:chart$number_of_antigens, function(no) { paste("AG", format(no - 1, justify="right", width=2), ags[[no]]$name, collapse=" ") }), collapse="\n")
-write("\n", stderr())
-write(length(r_antigen_list), stderr())
+r_antigen_list <- sapply(1:chart$number_of_antigens, function(no) { paste("AG", format(no - 1, justify="right", width=2), ags[[no]]$name, collapse=" ") })
 
+## r_passage_list <- sapply(1:chart$number_of_antigens, function(no) { paste("AG", format(no - 1, justify="right", width=2), ags[[no]]$passage, collapse=" ") })
+## write("\n", stderr())
+## write(r_passage_list, stderr())
+
+# ======================================================================
+
+test_that("number of antigens", { expect_equal(chart$number_of_antigens, 22) })
 test_that("antigen list", { expect_equal(ad_antigen_list, r_antigen_list) })
