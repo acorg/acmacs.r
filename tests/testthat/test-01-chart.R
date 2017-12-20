3706e18d8018a343f521c2cea0679ecaa60e5a7b
Eugene Skepner <github@skepner.eu>
2017-12-20 11:16:01 +0100

    tests development

--- tests/testthat/test-01-chart.R
+++ tests/testthat/test-01-chart.R
@@ -1,6 +1,13 @@
 context("Test chart")
-chart <- new(acmacs.Chart, "test-2004-3.ace")
-test_that("number of antigens", { expect_equal(chart$number_of_antigens, 22) })
-test_that("number of sera", { expect_equal(chart$number_of_sera, 6) })
-test_that("number of points", { expect_equal(chart$number_of_points, chart$number_of_antigens + chart$number_of_sera) })
-test_that("number of projections", { expect_equal(chart$number_of_projections, 1) })
+
+chart1 <- new(acmacs.Chart, "2004-3.ace")
+test_that("number of antigens", { expect_equal(chart1$number_of_antigens, 22) })
+test_that("number of sera", { expect_equal(chart1$number_of_sera, 6) })
+test_that("number of points", { expect_equal(chart1$number_of_points, chart1$number_of_antigens + chart1$number_of_sera) })
+test_that("number of projections", { expect_equal(chart1$number_of_projections, 1) })
+
+chart2 <- new(acmacs.Chart, "cdc-h1pdm-2009.acd1.bz2")
+test_that("number of antigens", { expect_equal(chart2$number_of_antigens, 303) })
+test_that("number of sera", { expect_equal(chart2$number_of_sera, 31) })
+test_that("number of points", { expect_equal(chart2$number_of_points, chart2$number_of_antigens + chart2$number_of_sera) })
+test_that("number of projections", { expect_equal(chart2$number_of_projections, 1) })
--- tests/testthat/test-02-antigens.R
+++ tests/testthat/test-02-antigens.R
@@ -1,18 +1,27 @@
 context("Test chart$antigens")
 
-ad_antigen_list <- system("ad chart-names test-2004-3.ace | grep '^AG'", intern=TRUE)
-#write(antigen_list, stderr())
+test_chart <- function(filename, expected_num_antigens, test_passages) {
+    ad_antigen_list <- system(paste("ad chart-names", filename, " | grep '^AG'"), intern=TRUE)
+                                        #write(antigen_list, stderr())
 
-chart <- new(acmacs.Chart, "test-2004-3.ace")
+    chart <- new(acmacs.Chart, filename)
 
-ags <- chart$antigens
-r_antigen_list <- sapply(1:chart$number_of_antigens, function(no) { paste("AG", format(no - 1, justify="right", width=2), ags[[no]]$name, collapse=" ") })
+    ags <- chart$antigens
+    r_antigen_list <- sapply(1:chart$number_of_antigens, function(no) {
+        paste("AG", format(no - 1, justify="right", width= if (expected_num_antigens <100) 2 else 3), ags[[no]]$full_name, paste0("[", ags[[no]]$date, "]"), collapse=" ")
+    })
 
-## r_passage_list <- sapply(1:chart$number_of_antigens, function(no) { paste("AG", format(no - 1, justify="right", width=2), ags[[no]]$passage, collapse=" ") })
-## write("\n", stderr())
-## write(r_passage_list, stderr())
+    test_that("number of antigens", { expect_equal(chart$number_of_antigens, expected_num_antigens) })
+    test_that("antigen list", { expect_equal(ad_antigen_list, r_antigen_list) })
+
+    if (test_passages) {
+        r_passage_list <- sapply(1:chart$number_of_antigens, function(no) { paste("AG", format(no - 1, justify="right", width=2), ags[[no]]$passage, collapse=" ") })
+        write("\n", stderr())
+        write(r_passage_list, stderr())
+    }
+}
+
+test_chart("2004-3.ace", 22, FALSE)
+test_chart("cdc-h1pdm-2009.acd1.bz2", 303, TRUE)
 
 # ======================================================================
-
-test_that("number of antigens", { expect_equal(chart$number_of_antigens, 22) })
-test_that("antigen list", { expect_equal(ad_antigen_list, r_antigen_list) })
