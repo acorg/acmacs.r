4baeb0d705dc655f393d35e7286410d84fc81d66
Eugene Skepner <github@skepner.eu>
2018-03-27 13:28:09 +0200

    grid test docs

--- tests/testthat/test-13-grid-test.R
+++ tests/testthat/test-13-grid-test.R
@@ -5,7 +5,11 @@ test_chart <- function(filename, expected_rows) {
     g1 <- new(acmacs.GridTest, c1)
     r1 <- g1$test()
     test_that(paste("number of hemi and trapped points in", filename), { expect_equal(nrow(r1), expected_rows) })
-    print(r1)
+    if (nrow(r1) > 0) {
+        diags <- unique(r1["diagnosis"])
+        # print(diags)
+        test_that(paste("only hemi points in", filename), { expect_true(diags == list("hemisphering")) })
+    }
 }
 
 test_chart("2004-3.ace", 0)
