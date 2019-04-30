d0dd895b9c643b44f4862c98cad83d2402fe69b6
Eugene Skepner <github@skepner.eu>
2019-04-30 10:34:54 +0200

    trying to resolve libomp conflict issue

--- tests/test-omp.R
+++ tests/test-omp.R
@@ -1,9 +1,20 @@
+# KMP_DUPLICATE_LIB_OK=TRUE
+
 # library(acmacs.r)
 library(acmacs.r, lib.loc="~/R/library")
-rs_ <- rowSums(data.frame(a=c(3,3,3),b=c(4,4,4)))
-cat("rowSums executed\n\n")
+
+rs <- function() {
+    rs_ <- rowSums(data.frame(a=c(3,3,3),b=c(4,4,4)))
+    cat("\nrowSums executed: ")
+    cat(rs_)
+    cat("\n\n")
+}
+
+rs()
 chart <- new(acmacs.Chart, "testthat/cdc-h1pdm-20090612.ace")
 cat("chart loaded\n")
 cat(chart$info)
 cat("\n\nabout to call chart$relax_many()\n\n")
-chart$relax_many("1280", 2, 10, TRUE)
+chart$relax_many("none", 2, 10, TRUE)
+cat(chart$info)
+rs()
